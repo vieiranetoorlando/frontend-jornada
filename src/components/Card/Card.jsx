2f8a450897213acba449c96578275cd6e0321b1f
ORLANDO VIEIRA NETO <110175293+vieiranetoorlando@users.noreply.github.com>
2024-01-25 15:33:29 +0000

    novos elementos

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,16 @@
+function Card () {
+    return (
+        <>
+          <div className='card'>
+            <h2>Rick Sanchez</h2>
+            <h3>Status Vivo</h3>
+            <h3>Espécie: Humana </h3>
+            <h3>Origem: Terra C-137</h3>
+            <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
+          </div>
+        </>
+      )
+}
+
+export default Card
+
