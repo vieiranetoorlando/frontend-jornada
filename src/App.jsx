3c8dceb0ab95d4ae661895ec820fe4b0405fa642
ORLANDO VIEIRA NETO <110175293+vieiranetoorlando@users.noreply.github.com>
2024-01-30 13:41:43 +0000

    adicionando cards

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,22 @@ function App() {
     name: 'Rick Sanchez',
     image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
   }
+
+  const item2 = {
+    name: 'Morty Smith',
+    image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+  }
+
+  const item3 = {
+    name: 'Summer Smith',
+    image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
+  }
   return (
+    
     <>
     <Card item={item1} />
-    <Card item={item1} />
+    <Card item={item2} />
+    <Card item={item3} />
 
     </>
   )
