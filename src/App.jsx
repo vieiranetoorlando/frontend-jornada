dbcaab67c14327d9a555c291b5a2ddff4a17fead
ORLANDO VIEIRA NETO <110175293+vieiranetoorlando@users.noreply.github.com>
2024-01-31 14:39:56 +0000

    Atualizando o estado"itens" com os resultado da API

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,37 @@
+import { useState } from 'react'
 import './App.css'
 import Card from './components/Card/Card'
 
 function App() {
-  const item1 = {
-    name: 'Rick Sanchez',
-    image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
-  }
 
-  const item2 = {
-    name: 'Morty Smith',
-    image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
-  }
 
-  const item3 = {
-    name: 'Summer Smith',
-    image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
-  }
-
-  const item4 = {
-    name: 'Beth Smith',
-    image: 'https://rickandmortyapi.com/api/character/avatar/4.jpeg'
-  }
-  const itens =[ item1, item2, item3, item4]
+  const [itens, setItens] = useState([])
 
   //Carregamento de dados da API (Backend)
   async function carregarDadosApi() {
+
+
+   
     //Declarar a UTL da API
   const apiURL = 'https://rickandmortyapi.com/api/character/'
+  
+
     //Preparar e executar a Requisição
   const response = await fetch(apiURL)
+
 
   //console.log(response)
 
   const body = await response.json()
 
-  console.log(body)
+  //console.log(body)
 
   // Extrair a propriedade results do body
   //Essa propiedade contém a lista de itens
   const results = body.results;
+
+  //Atualizando o estado"itens" com os resultado da API
+  setItens(results)
 
   }
 // Chamando a função que carrega os dados da API
